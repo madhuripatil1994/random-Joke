35c35567456e1e4cdb1b06355e09aad7381b6b5a
Patil <madhuri.patil@atos.net>
2019-03-05 15:50:17 +0530

    added

--- src/Containers/Joke-container/JokeContainer.js
+++ src/Containers/Joke-container/JokeContainer.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import{ axiosAPI } from '../../Utils/APIUtils';
+import axios from 'axios';
 
-class jokeContainer extends React.Component {
+
+class JokeContainer extends React.Component {
 
     constructor(props){
         super(props);
         this.state = {
-            joke : []
+            persons: []
         }
     }
 
-    componentWillMount(){
-        var joke = axiosAPI();
-        console.log(joke);
+    componentDidMount() {
+      this.getJokes();
     }
 
+    getJokes(){
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
+
+    com
+    
+    
     render(){
-        return(<div></div>)
+        return(
+            <div className="card text-center">
+            <div className="card-header">
+              Funny Jokes!!
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Joke Id :</h5>
+              <p className="card-text"> { this.state.persons.map(person => <li>{person.name}</li>)}</p>
+              <a href="#" className="btn btn-primary" onClick ={this.getJokes.bind(this)}>Next Joke</a>
+            </div>
+            <div className="card-footer text-muted">
+             @CopyRights - All rights reserved. Created by
+            </div>
+          </div>
+        )
     }
 
 }
 
-export default jokeContainer;
+export default JokeContainer;
--- src/Containers/main-container/MainContainer.js
+++ src/Containers/main-container/MainContainer.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import{ axiosAPI } from '../../Utils/APIUtils';
+import JokeContainer from '../Joke-container/JokeContainer'
 
 class MainContainer extends React.Component {
 
     render(){
          return(
-            <div class="card text-center">
-            <div class="card-header">
-              Funny Jokes!!
+            <div className="card text-center">
+              <JokeContainer />
             </div>
-            <div class="card-body">
-              <h5 class="card-title">Joke Id :</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Next Joke</a>
-            </div>
-            <div class="card-footer text-muted">
-             @CopyRights - All rights reserved. Created by
-            </div>
-          </div>
+            
          )
     }
 
